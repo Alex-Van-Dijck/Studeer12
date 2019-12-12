9bf880c4323171d959a4d447b1783ace685e2d91
migue <Miguel.reyntjens@student.kdg.be>
2019-12-12 14:32:20 +0100

    Test afgehandeld

--- src/Vragen.java
+++ src/Vragen.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class Vragen {
-    // test van de miguel
-//    String[] Frans = {"Vraag1", "Vraag2","Vraag3","Vraag4","Vraag5"};
+    // String[] Frans = {"Vraag1", "Vraag2","Vraag3","Vraag4","Vraag5"};
     List<String> Vragen = new ArrayList();
 }
