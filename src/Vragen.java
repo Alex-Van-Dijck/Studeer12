acebfd4eabdcd104fb2428ccfecfa0261f833db4
Alex-Van-Dijck <coolealman@gmail.com>
2019-12-12 14:29:29 +0100

    Init test

--- src/Vragen.java
+++ src/Vragen.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Vragen {
+    // test van de miguel
 //    String[] Frans = {"Vraag1", "Vraag2","Vraag3","Vraag4","Vraag5"};
     List<String> Vragen = new ArrayList();
 }
