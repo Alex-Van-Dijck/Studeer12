75484c92d3ad1a5b7843f9797b452ab98349e87d
Alex-Van-Dijck <coolealman@gmail.com>
2019-12-12 14:34:07 +0100

    12/12/2019 14:23

--- src/Vraag.java
+++ src/Vraag.java
@@ -1,5 +1,5 @@
 public class Vraag {
     public Vraag(String Question) {
-        Vragen.add()
+
     }
 }
--- src/Vragen.java
+++ src/Vragen.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Vragen {
+    // Test Alex
     // String[] Frans = {"Vraag1", "Vraag2","Vraag3","Vraag4","Vraag5"};
     List<String> Vragen = new ArrayList();
 }
