45b7241c4f784086ce13f1e12ee0e6370ac56b67
Alex-Van-Dijck <coolealman@gmail.com>
2019-12-14 13:45:26 +0100

    maakVraag() af maximum & gemiddelde met 2 of 3

--- src/Vragen.java
+++ src/Vragen.java
@@ -11,7 +11,16 @@ public class Vragen {
 
     //Automatische vragen
         //Automatische Maximumvraag
-    public void maakMaximumvraag() {
+    private void maakMaximumvraag3() {
+
+        int random1 = rand.nextInt(100) + 1;
+        int random2 = rand.nextInt(100) + 1;
+        int random3 = rand.nextInt(100) + 1;
+        int hoogste = Math.max(random1, Math.max(random2, random3));
+        vragenStatistiek.add("Welk getal is hoger? " + random1 + " of " + random2 + "?" );
+        antwoordenStatistiek.add(Integer.toString(hoogste));
+    }
+    private void maakMaximumvraag2() {
 
         int random1 = rand.nextInt(100) + 1;
         int random2 = rand.nextInt(100) + 1;
@@ -20,14 +29,43 @@ public class Vragen {
         antwoordenStatistiek.add(Integer.toString(hoogste));
     }
         //Automatische gemiddeldevraag
-    public void maakGemiddeldevraag(){
-        int random1 = rand.nextInt(100) + 1;
-        int random2 = rand.nextInt(100) + 1;
-        int random3 = rand.nextInt(100) + 1;
-        int gemiddelde = (random1 + random2 + random3)/3;
+        private void maakGemiddeldevraag3(){
+        double random1 = rand.nextInt(100) + 1;
+        double random2 = rand.nextInt(100) + 1;
+        double random3 = rand.nextInt(100) + 1;
+        double gemiddelde = (random1 + random2 + random3)/3;
 
         vragenStatistiek.add("Wat is het gemiddelde van " + random1 + ", " + random2 + " en "  + random3 + "?" );
-        antwoordenStatistiek.add(Integer.toString(gemiddelde));
+        antwoordenStatistiek.add(Double.toString(gemiddelde));
+    }
+    private void maakGemiddeldevraag2(){
+        double random1 = rand.nextInt(100) + 1;
+        double random2 = rand.nextInt(100) + 1;
+        double gemiddelde = (random1 + random2)/2;
+
+        vragenStatistiek.add("Wat is het gemiddelde van " + random1 + " en " + random2 + "?" );
+        antwoordenStatistiek.add(Double.toString(gemiddelde));
+    }
+
+
+    //Random vraag selector
+    public void maakVraag(){
+        int random = rand.nextInt(4) + 1;
+
+        switch(random){
+            case 1:
+                maakGemiddeldevraag2();
+                break;
+            case 2:
+                maakGemiddeldevraag3();
+                break;
+            case 3:
+                maakMaximumvraag2();
+                break;
+            case 4:
+                maakMaximumvraag3();
+                break;
+        }
     }
 
 
