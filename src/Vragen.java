dd3620bb49102a268a9fffafbae0065ffa645b53
Alex-Van-Dijck <coolealman@gmail.com>
2019-12-14 14:10:09 +0100

    Automatische gemiddeldevraag

--- src/Vragen.java
+++ src/Vragen.java
@@ -19,6 +19,16 @@ public class Vragen {
         vragenStatistiek.add("Welk getal is hoger? " + random1 + " of " + random2 + "?" );
         antwoordenStatistiek.add(Integer.toString(hoogste));
     }
+        //Automatische gemiddeldevraag
+    public void maakGemiddeldevraag(){
+        int random1 = rand.nextInt(100) + 1;
+        int random2 = rand.nextInt(100) + 1;
+        int random3 = rand.nextInt(100) + 1;
+        int gemiddelde = (random1 + random2 + random3)/3;
+
+        vragenStatistiek.add("Wat is het gemiddelde van " + random1 + ", " + random2 + " en "  + random3 + "?" );
+        antwoordenStatistiek.add(Integer.toString(gemiddelde));
+    }
 
 
 
